04e1adbe9c02b88a5414261b235206686b5573f7
Christoph Hausner <christoph_hausner@hotmail.de>
2023-08-11 22:15:27 +0200

    Add runStatsOneVsRandomBots helper, cleanup Program.fs

--- UnoAI/Program.fs
+++ UnoAI/Program.fs
@@ -1,31 +1,63 @@
-open Game
+﻿open Game
 open BotRunner
 open RandomBot
 open GreedyBot
+open DiversityBot
 open CardRankingBot
 open MixBot
-open DiversityBot
 open ScoreBot
 open CardCountingBot
+
+let runStatsOneVsRandomBots numPlayers =
+    let ruleSet = defaultRuleSet
+    let bots =
+        [| RandomBot.Factory()
+           GreedyBot.Factory(GreedyBot.DefaultSettings)
+           DiversityBot.Factory(DiversityBot.DefaultSettings)
+           CardRankingBot.Factory(CardRankingBot.DefaultSettingsWinRate)
+           CardRankingBot.Factory(CardRankingBot.DefaultSettingsAvgPoints)
+           MixBot.Factory(MixBot.DefaultSettingsWinRate)
+           MixBot.Factory(MixBot.DefaultSettingsAvgPoints)
+           ScoreBot.Factory(ScoreBot.DefaultSettingsWinRate)
+           ScoreBot.Factory(ScoreBot.DefaultSettingsAvgPoints)
+           CardCountingBot.Factory(CardCountingBot.DefaultSettingsWinRate)
+           CardCountingBot.Factory(CardCountingBot.DefaultSettingsAvgPoints)|]
+    let randomizePlayOrder = true
+    let timeout = 1000
+    let numGames = 1_000_000
+    let bot2 = RandomBot.Factory()
+
+    printfn "%d players, * vs. %d RandomBot(s)" numPlayers (numPlayers - 1)
+    printfn "%-18s %10s %13s" "Bot" "Win rate" "Avg. points"
+
+    for bot1 in bots do
+        let stats = initStats numPlayers
+        let bots' = bot1 :: (List.replicate (numPlayers - 1) bot2) |> List.toArray
+        runBotsBatch ruleSet bots' randomizePlayOrder timeout numGames
+        |> Seq.iter (fun result -> updateStats stats result)
+        let botName1 = (bot1 (new Game.Game(defaultRuleSet, 2, 0)) 0).GetType().Name
+        let winRate = (float stats.NumGamesWon[0]) / (float stats.NumGames)
+        let averagePoints = (float stats.TotalPoints[0]) / (float stats.NumGames)
+        printfn "%-18s %10.3f %13.1f" botName1 winRate averagePoints
+
+    printfn ""
 
 [<EntryPoint>]
 let main argv =
     let ruleSet = defaultRuleSet
     let bots =
         [| RandomBot.Factory()
-           RandomBot.Factory()
-           //GreedyBot.Factory(GreedyBot.DefaultSettings)
-           //DiversityBot.Factory(DiversityBot.DefaultSettings)
-           //CardRankingBot.Factory(CardRankingBot.DefaultSettingsWinRate)
+           GreedyBot.Factory(GreedyBot.DefaultSettings)
+           DiversityBot.Factory(DiversityBot.DefaultSettings)
+           CardRankingBot.Factory(CardRankingBot.DefaultSettingsWinRate)
            MixBot.Factory(MixBot.DefaultSettingsWinRate)
-           //ScoreBot.Factory(ScoreBot.DefaultSettingsWinRate)
-           //CardCountingBot.Factory(CardCountingBot.DefaultSettingsWinRate) |]
-           |]
+           ScoreBot.Factory(ScoreBot.DefaultSettingsWinRate)
+           CardCountingBot.Factory(CardCountingBot.DefaultSettingsWinRate) |]
     let randomizePlayOrder = true
     let timeout = 1000
     let numGames = 1_000_000
 
-    //runBatchAndPrintStats ruleSet bots randomizePlayOrder timeout numGames
+    runBatchAndPrintStats ruleSet bots randomizePlayOrder timeout numGames
 
     //CardRankingBotOptimization.optimizeRanking()
     //CardRankingBotOptimization.optimizeCardCountLimits()
@@ -38,6 +70,8 @@ let main argv =
 
     //CardCountingBotOptimization.optimizeWeights()
 
-    MixBot.optimizeDiversityWeight()
+    // generate statistics for README.md
+    //runStatsOneVsRandomBots 2
+    //runStatsOneVsRandomBots 6
 
     0
